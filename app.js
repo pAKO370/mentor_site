c9af1a9041b538d57c0be388f7b954d960c06611
Phil Fluegel <pfluegelcx@gmail.com>
2016-06-23 11:51:58 -0500

    trying changes to get it to work in production

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 var express = require("express");
 var app = express();
+var stormpath = require('express-stormpath');
+
 app.use(express.static("public"));
-app.listen(process.env.PORT || 8080);
+app.use(stormpath.init(app, { }));
+
+
+
+app.on('stormpath.ready', function () {
+  app.listen(3000, function () {
+    //...
+  });
+});
